f91ba62883a88bca0dde05e289692f4c3179d00e
Roman-Yaremchuk <yaremchukroma5@gmail.com>
2024-06-10 20:17:34 +0200

    Create branch 'button' and add project.

--- src/main.cpp
+++ src/main.cpp
@@ -1,175 +1,192 @@
-  #include <Arduino.h>
-  #include <WiFi.h>
-  #include <WebServer.h>
-  #include <HTTPClient.h>
+#include <Adafruit_NeoPixel.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
 
-  // Konfiguracja punktu dostępowego
-  const char* ap_ssid = "MGD_Base";  // Nazwa SSID punktu dostępowego
-  const char* ap_password = "12345678";  // Hasło do punktu dostępowego
-  WebServer server(80);  // Tworzy obiekt serwera HTTP na porcie 80
+// Definicja pinów
+const int buttonPin = 2; // Pin, do którego podłączony jest przycisk
+const int np_pin = 4;    // Pin, do którego podłączony jest NeoPixel
 
-  // Zmienne globalne i flagi
-  String message = "No data received";  // Zmienna globalna przechowująca wiadomość
-  int httpCode = 0;
-  int httpStartCode = 0; //!! powinno być 0
-  int numStations = 0; // ilość podlączonych urządzeń
+// Zmienne globalne
+const char *ssid = "MGD_Base";
+const char *password = "12345678";
+const char *serverClickBotton1 = "http://192.168.4.1/1001";
+int         httpCodeIn = 0;
+const int   count_pixels = 12;
 
-  const unsigned long delayConnectTime = 5000; //- dla podłączonych urządzeń
-  int flagDelayConnect  = 0; //- dla podłączonych urządzeń
-  int flagStartGame = 1; //!! powinno być 0  flag ze gra jest rozpoczęta
-
-  // Zmienne do Szpital type1
-    int numHealthTeam1 = 5; //!! powinno być 0
-    int numHealthTeam2 = 5;  //!! powinno być 0
-    //unsigned long  timeGame = 0;
-    //unsigned long  respDelayGame = 0;
+// flags
+bool flagConnectingDevice = false;
+bool flagStartGame = true;
+bool flagGetMessageToServer = false;
+bool lastButtonState = HIGH; // Poprzedni stan przycisku
 
 
-  void getFinish()
-  {
-    message = "getFinish - jest zapytanie";  // Aktualizuje zmienną message
-    if(numHealthTeam1 <= 0 || numHealthTeam2 <= 0)
-      {
-        server.send(11005);
-        flagStartGame = 0;
-          message = "getFinish - wysłany kod";  // Aktualizuje zmienną message
-      }
+// Tworzenie obiektów klas
+Adafruit_NeoPixel pixels(count_pixels, np_pin, NEO_GRB + NEO_KHZ800);
 
-    Serial.println(message);
-  }
-  // Funkcja obsługująca żądania HTTP na głównym URI ("/")
-  void handleRoot() 
-  {
-    server.send(200, "text/plain", message);  // Wysyła odpowiedź HTTP 200 z treścią message
-  }
+// deklaracja funkcji
+void connectedDevice(int user_delay, bool* flag1);
+void trueConnect(bool* flag1);
+void clickButton(bool* flag1, bool* flag2);
+void send_acceptMessageServer(bool* flag1, bool* flag2, bool* flag3);
 
-  // Funkcja obsługująca żądania HTTP na URI "/button1"
-  void handleButton1() 
-  {
-    message = "Button 1 Pressed!";  // Aktualizuje zmienną message
-    if(numHealthTeam1 > 0)
-    {
-      numHealthTeam1--;
-    server.send(1501, "text/plain", String(numHealthTeam1));  // Wysyła odpowiedź HTTP 200 z treścią "OK"
-    } else
-    {
-      getFinish();
-    }
-  Serial.println("Button 1 Pressed!");
-  Serial.println("Health updated: Team1 = " + String(numHealthTeam1) + ", Team2 = " + String(numHealthTeam2) + "Game = " + String(flagStartGame));
+void setup()
+{
+    // Ustawienie pinu przycisku jako wejście z wewnętrznym rezystorem pull-up
+    pinMode(buttonPin, INPUT_PULLUP);
+    // Podłączenie do serwera
+    WiFi.begin(ssid, password);
+    // Tworzenie instancji NeoPixel
+    pixels.begin();
+    // Inicjalizacja komunikacji szeregowej i WiFi
+    Serial.begin(115200); 
+}
 
-  }
+void loop()
+{
+    // sprawdzanie połączenia
+    trueConnect(&flagConnectingDevice);  
+    // wyświetlane statusu (lub podtwierdzenia) połączenia na neo_pixel
+    connectedDevice(100, &flagConnectingDevice);
+    //obsługa kliknięcia przycisku
+    clickButton(&flagStartGame, &flagGetMessageToServer);
+    //osługa wysłania i przyjęcia danych z servera
+    send_acceptMessageServer(&flagGetMessageToServer, &flagConnectingDevice, &flagStartGame);
+    
+}
 
-  // Funkcja obsługująca żądania HTTP na URI "/button2"
-  void handleButton2() 
-  {
-    message = "Button 2 Pressed!";  // Aktualizuje zmienną message
-    if(numHealthTeam2 > 0)
-    {
-      numHealthTeam1--;
-    server.send(2501, "text/plain", String(numHealthTeam2));  // Wysyła odpowiedź HTTP 200 z treścią "OK"
-    } else
-    {
-      getFinish();
-    }
-    Serial.println("Button 2 Pressed!");
-    Serial.println("Health updated: Team1 = " + String(numHealthTeam1) + ", Team2 = " + String(numHealthTeam2) + "Game = " + String(flagStartGame));
-  }
+//init: tmr1, tmr3
+void connectedDevice(int user_delay, bool *flag1)
+{
+    static bool wasConnected = false; // Flaga do śledzenia poprzedniego stanu połączenia
 
-
-  // Pokazuję i wysyła dane o ilości urządzeń na ekran
-  void showConnectedDevices() 
-  {
-  static unsigned long previousMillis = 0; // Zmienna do przechowywania poprzedniego czasu
-  unsigned long currentMillis = millis(); // Pobranie bieżącego czasu
-      if (currentMillis - previousMillis >= delayConnectTime) 
-      {
-      previousMillis = currentMillis; 
-      numStations = WiFi.softAPgetStationNum();
-      Serial.print("Number of connected devices: ");
-      Serial.println(numStations);
-      } 
-  }
-  // odczytuje znaczenie kodu gry i zapisuje go do httpStartCode
-  void httpCodeStartRead( )
-  {
-  if (WiFi.status() == WL_CONNECTED) 
-      {
-        HTTPClient http;
-        httpStartCode = http.GET();
-        if (httpStartCode > 0) 
+    if ((WiFi.status() != WL_CONNECTED) && (*flag1 == false))
+    { 
+        static unsigned long tmr1 = 0; // Zmienna do przechowywania poprzedniego czasu
+        static int currentPixel = 0;   // Aktualnie obsługiwany indeks diody
+        
+        // Sprawdzenie, czy upłynął określony czas
+        if (millis() - tmr1 >= user_delay)
         {
-          flagStartGame = 1;
-          Serial.println("Start Game ");
+            // Zaktualizowanie poprzedniego czasu
+            tmr1 = millis(); 
+            // Wyczyść wszystkie diody
+            pixels.clear();
+            // Włącz diodę o aktualnym indeksie
+            pixels.setPixelColor(currentPixel, pixels.Color(255, 255, 255));
+            // Wyświetl ustawione kolory
+            pixels.show();
+            Serial.println(".");
+            // Przełącz do następnej diody
+            currentPixel++;
+            if (currentPixel >= count_pixels)
+            {
+                currentPixel = 0; // Wróć do pierwszej diody po osiągnięciu ostatniej
+            }
         }
-        http.end();
-      }
-  }
+        wasConnected = false; // Ustawienie flagi na false, ponieważ połączenie jest utracone
+                              // lub nawiązywane po raz pierwszy(dla funkcji bieżącej).
+    } 
+    else 
+    {
+        *flag1 = true; //zmiana flagi(połączenie nawiązane) dla funkcji zewnętrznych         
+        if (wasConnected == false)
+        {
+            wasConnected = true; // Ustawienie flagi, że połączenie zostało nawiązane(dla funkcji bieżącej)
 
-  // Funkcja obsługująca żądania HTTP na URI "/setHealth"
-  void startGameOnBase() 
-  {
-    if (server.hasArg("team1") && server.hasArg("team2") && server.hasArg("startGameFlag")) 
-    {
-      numHealthTeam1 = server.arg("team1").toInt();
-      numHealthTeam2 = server.arg("team2").toInt();
-      flagStartGame = server.arg("startGameFlag").toInt();
-      message = "Start updated";
-      server.send(200, "text/plain", "Health updated");
-      Serial.println("Health updated: Team1 = " + String(numHealthTeam1) + ", Team2 = " + String(numHealthTeam2) + "Game = " + String(flagStartGame));
-    } else 
-    {
-      server.send(400, "text/plain", "Bad Request");
+            unsigned long tmr3 = millis(); // Zmienna do przechowywania poprzedniego czasu startowego
+            // Ustawienie diod na zielono i wyświetlenie ich
+            pixels.fill(pixels.Color(0, 255, 0));
+            pixels.show();
+            // Sprawdzenie, czy upłynęła 1 sekunda
+            while (millis() - tmr3 < 1000)
+            {
+                // Poczekaj 1 sekundę
+            }
+            pixels.clear(); // Wyczyść wszystkie diody po 1 sekundzie
+            pixels.show();  // Pokaż zmiany
+            Serial.println("    Connected"); //Wyprowadzenie statusu na serial_port
+        }
     }
-  }
+}
 
-  void GameSzpitalType1()
-  {
-  startGameOnBase();
-  Serial.println("Game ////");
-  Serial.println(String(numHealthTeam1));
-  Serial.println(String(numHealthTeam2));
-  Serial.println("Health updated: Team1 = " + String(numHealthTeam1) + ", Team2 = " + String(numHealthTeam2) + "Game = " + String(flagStartGame));
-
-
-  }
-
-
-  // Funkcja setup, wykonywana raz na początku
-  void setup() 
-  {
-    Serial.begin(115200);  // Inicjalizuje komunikację szeregową z prędkością 115200 bps
-      // Ustawienia punktu dostępowego
-    WiFi.softAP(ap_ssid, ap_password);  // Tworzy punkt dostępowy z podanym SSID i hasłem
-      IPAddress IP = WiFi.softAPIP();  // Pobiera adres IP punktu dostępowego
-    Serial.print("AP IP address: ");
-    Serial.println(IP);  // Wyświetla adres IP punktu dostępowego
-
-    // Rejestruje funkcje obsługi żądań HTTP dla określonych URI
-    server.on("/", handleRoot);  // Dla głównego URI ("/")
-    server.on("/btn1Clk", handleButton1);  // Dla URI "/button1"
-    server.on("/btn2Clk", handleButton2);  // Dla URI "/button2"
-    //server.on("/2001", handleButton2);  // Dla URI "/button2"
-    server.on("/Game01/start", HTTP_POST, startGameOnBase);  // Dla URI "/setHealth"
-    server.on("/startGame", GameSzpitalType1);  // Dla URI "/setHealth"
-    server.on("/getFinish", getFinish); // pytanie czy koniec gry.
-    server.begin();  // Uruchamia serwer HTTP
-    Serial.println("HTTP server started");  // Wypisuje "HTTP server started" po uruchomieniu serwera
-  }
-
-  // Funkcja loop, wykonywana wielokrotnie w nieskończonej pętli
-  void loop() 
-  {
-    server.handleClient();  // Obsługuje żądania klientów HTTP
-
-    showConnectedDevices();// Czeka i wyświetla liste podłączonych urządzeń
-
-   if ( numStations > 0 && flagStartGame == 0)
-   {
-    httpCodeStartRead( );
-   }
-   if ( numStations > 0 && httpStartCode == 12001 && flagStartGame == 1)
+//init: tmr1,tmr3,(new)tmr2
+void trueConnect(bool* flag1)
+{
+    static unsigned long tmr2 = 0;
+    if(millis() - tmr2 >= 1000)
     {
-      GameSzpitalType1();
+        tmr2 = millis();
+        if(WiFi.status() != WL_CONNECTED)
+        {
+            Serial.println("try connect");
+            *flag1 = false;
+        }
     }
-  }
+}
+
+//init: tmr1, tmr2,tmr3, (new)tmr4
+void clickButton(bool* flag1, bool* flag2)
+{
+    if(*flag1)
+    {
+        static unsigned long tmr4 = 0;
+        bool buttonState = digitalRead(buttonPin);
+
+        if (millis() - tmr4 > 50)
+        {
+            tmr4 = millis();
+            if (!buttonState && lastButtonState)
+            {
+                Serial.println("Pressed button");
+                *flag2 = true;
+                // Zmienna do przechowywania poprzedniego czasu startowego
+                unsigned long tmr5 = millis();
+                // Ustawienie diod na czrwono i wyświetlenie ich
+                if (WiFi.status() == WL_CONNECTED)//
+                {
+                    pixels.fill(pixels.Color(255, 0, 0));
+                    pixels.show();
+                    // Sprawdzenie, czy upłynęła 1 sekunda
+                    while (millis() - tmr5 < 1000)
+                    {
+                        // Poczekaj 1 sekundę
+                    }
+                    pixels.clear();                    // Wyczyść wszystkie diody po 1 sekundzie
+                    pixels.show();                     // Pokaż zmiany
+                    Serial.println("neo_pixel light"); // Wyprowadzenie podtwierdzenia zaświecenia neo_pixel na serial_port
+                }
+            }
+            lastButtonState = buttonState;
+        }
+    }
+}
+
+void send_acceptMessageServer(bool* flag1, bool* flag2, bool* flag3 )
+{
+    if(*flag1 && *flag2 && flag3 ) //jeżeli przycisk został wciśnięty(*flag1(flagGetMessageToServer))
+    {
+        Serial.println("Wysłanie wiadomości na serwer");
+        Serial.println();
+        if (WiFi.status() == WL_CONNECTED)
+            {
+                HTTPClient http;
+                http.begin(serverClickBotton1);
+                httpCodeIn = http.GET();
+                if (httpCodeIn == 1001)
+                {
+                    Serial.println("Button 1 pressed, sent to server");
+                }else if(httpCodeIn == 1002)
+                {
+                    Serial.println("dane z servera(2), sent to server");
+                }
+                http.end();
+            }
+            *flag1 = false;
+    }
+    /*
+    *
+    *
+    *Kod do interakcji z serverem
+    *
+    */
+}
