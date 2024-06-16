ba5bf5642aa96cd9a6ce0e62067715ccfbce26d6
viktorlasko <viktorlasko.te@gmial.com>
2024-06-16 15:45:58 +0200

    Poprawi display

--- src/display.cpp
+++ src/display.cpp
@@ -4,45 +4,18 @@
 const char* ssid = "MGD_Base";
 const char* password = "12345678";
 const char* serverName = "http://192.168.4.1/12001"; // Zmodyfikuj adres IP serwera
-const char* serverIP = "192.168.4.1";  // Zmień na adres IP punktu dostępowego ESP32
+const char* game01Start = "http://192.168.4.1/Game01/start";  // Zmień na adres IP punktu dostępowego ESP32
 const int buttonPin = 15; 
 
+//Game01 - szpital tylko zdrowie ważne
 int gameType = 1; // typ gry
 // Zmienne zdrowia drużyn
 int team1Health = 20;
 int team2Health = 20;
 
-// Funkcja do wysyłania danych zdrowia
-void sendToBaseStartGame01(int team1, int team2) 
-{
-  if (WiFi.status() == WL_CONNECTED) 
-    {
-    HTTPClient http;
-    String url = String("http://") + serverIP + "/Game01/start";
-    http.begin(url);
-    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
 
-    String payload = "team1=" + String(team1) + "&team2=" + String(team2) + "&startGameFlag=1";
-    int httpResponseCode = http.POST(payload);
-
-    if (httpResponseCode > 0) 
-      {
-        String response = http.getString();
-        Serial.print("HTTP Response code: ");
-        Serial.println(httpResponseCode);
-        Serial.print("Response: ");
-        Serial.println(response);
-      } else 
-      {
-        Serial.print("Error code: ");
-        Serial.println(httpResponseCode);
-      }
-    http.end();
-    } else 
-    {
-      Serial.println("WiFi Disconnected");
-    }
-}
+// Deklaracji funkcji
+void sendToBaseStartGame01(int team1, int team2);
 
 
 void setup() 
@@ -66,4 +39,36 @@ void loop() {
     delay(1000); // Debounce
   }
  
+}
+
+
+// Funkcja do wysyłania danych zdrowia i kodu startu gry
+void sendToBaseStartGame01(int team1, int team2) 
+{
+  if (WiFi.status() == WL_CONNECTED) 
+    {
+    HTTPClient http;
+    http.begin(game01Start);
+    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+
+    String payload = "team1=" + String(team1) + "&team2=" + String(team2) + "&startGameFlag=1";
+    int httpResponseCode = http.POST(payload);
+
+    if (httpResponseCode > 0) 
+      {
+        String response = http.getString();
+        Serial.print("HTTP Response code: ");
+        Serial.println(httpResponseCode);
+        Serial.print("Response: ");
+        Serial.println(response);
+      } else 
+      {
+        Serial.print("Error code: ");
+        Serial.println(httpResponseCode);
+      }
+    http.end();
+    } else 
+    {
+      Serial.println("WiFi Disconnected");
+    }
 }
